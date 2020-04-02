94175d7b9ff5bd360706a076f6c42a075c0b4a4f
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-04-03 00:26:21 +0300

    lesson06 basic task - game begin

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const isNum = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+let guessNumber = function() {
+    let numberRandom = Math.floor(Math.random() * 100);
+
+    function gameLoop() {
+        let numberUser = prompt('Угадай число от 1 до 100 ' + numberRandom, '');
+
+        console.log(numberUser);
+
+        if (numberUser === null) {
+            return;
+        }
+
+        if (!isNum(numberUser)) {
+            alert('Введи число!');
+            gameLoop();
+        }
+
+        if (numberUser > numberRandom) {
+            alert('Загаданное число меньше');
+            gameLoop();
+        } else if (numberUser < numberRandom) {
+            alert('Загаданное число больше');
+            gameLoop();
+        }
+    }
+
+    gameLoop();
+};
+
+guessNumber();
